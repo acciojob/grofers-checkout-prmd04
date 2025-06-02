a6f197a152278b31455a2607a01e26f569aa2d8d
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-06-02 15:38:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+const tprice = document.querySelectorAll(".price");
 
 const getSum = () => {
-//Add your code here
+	let fp=0;
+
+  for(let price of tprice){
+  fp += parseInt(price.innerText);
+  }
+  const displayPrice = document.createElement("div");
+  displayPrice.append(`Total Price = ${fp}`);
+  document.body.appendChild(displayPrice);
+  
   
 };
 
